cc31038fcc5f17033a779cd0ce71f841571dc05c
幻音丶小涛 <2289453456@qq.com>
2017-05-03 22:26:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>欢迎使用七空幻音授权登录SDK文件</title>
+	<link rel="shortcut icon" href="http://cos.acgxt.com/images/xt-icon.png" type="image/x-icon">
+	<style>
+		*{margin:0;padding:0;}
+		h2{
+			font-weight:200;
+			font-size:30px;
+			text-align:center;
+			color: #626262;
+		}
+		a{
+			margin:20px auto;
+			text-decoration:none;
+			display:block;
+			width:180px;
+		}
+		img{
+			width:180px;
+		}
+	</style>
+</head>
+<body>
+<h2>欢迎使用七空幻音授权登录SDK文件</h2>
+<h2>点击下方按钮即可开始授权登录</h2>
+<a href="./connect.php"><img src="login.png" alt="点击开始授权登录"></a>
+
+</body>
+</html>
